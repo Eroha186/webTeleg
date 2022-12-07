b1a233a77ae172116a4bb04ba101b4ce09eafdfe
eroha186 <dimon.s186@yandex.ru>
2022-12-07 15:06:11 +0700

    d

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full mx-auto container relative flex flex-col items-center top-1/2 -translate-y-1/2">
     <img alt="Vue logo" class="logo mx-auto" src="@/assets/logo.png"/>
-    <img alt="Vue logo" class="logo mx-auto" src="@/assets/dc.png"/>
+    <img alt="Vue logo" class="logo mx-auto" id="dc" src="@/assets/dc.png"/>
   </div>
 </template>
 
