c1b96560c91a64b6f05b2d2b326a0ea01d368668
eroha186 <dimon.s186@yandex.ru>
2022-12-07 13:15:02 +0700

    vite config

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="w-full mx-auto container relative top-1/2 -translate-y-1/2">
+  <div class="w-full mx-auto container relative flex flex-col items-center top-1/2 -translate-y-1/2">
+    <img alt="Vue logo" class="logo mx-auto" src="@/assets/logo.png"/>
     <img alt="Vue logo" class="logo mx-auto" src="@/assets/dc.png"/>
   </div>
 </template>
