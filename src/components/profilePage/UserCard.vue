80506911a56f4368f5ed5fb47f1a4b5f38415f15
eroha186 <dimon.s186@yandex.ru>
2022-12-08 17:22:24 +0700

    d

--- src/components/profilePage/UserCard.vue
+++ src/components/profilePage/UserCard.vue
@@ -3,12 +3,13 @@
     <div class="flex flex-col items-center pb-10">
       <div class="relative">
         <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="@/assets/user.jpg" alt="Bonnie image"/>
-        <div class="absolute flex flex-col items-center custom-border-color border-4 justify-center w-[37px] h-[37px] text-sm font-bold right-0 bottom-0 rounded-full bg-green-500 text-white">
+        <div class="absolute flex flex-col items-center custom-border-color border-4 justify-center w-[37px] h-[37px] text-sm font-bold right-0 bottom-0 rounded-full  text-white">
           <span>
             10%
           </span>
         </div>
       </div>
+      <div class="w-[100px] h-[100px] border-4 dark:border-[#103141]"></div>
       <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Bonnie Green</h5>
       <span class="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
     </div>
@@ -17,7 +18,6 @@
 
 <style scoped>
 .custom-border-color {
-  border-color: #103141;
-  opacity: 1;
+  border: 4px solid #103141
 }
 </style>
